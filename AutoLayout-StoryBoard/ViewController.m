34c87bcfb09af767d46e323f2d22fb7e2b0b1b52
HighGroundLover <kclarkoneil@yahoo.ca>
2018-08-07 18:59:20 -0700

    Initial Commit

---
+++ AutoLayout-StoryBoard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AutoLayout-StoryBoard
+//
+//  Created by Kit Clark-O'Neil on 2018-08-07.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
